c7b33383cf146589694db41f8d0e468d9a40c2af
MoatazAbdAlmageed <moataz@wuilt.com>
2020-07-01 18:09:39 +0200

    add `weather`

---
+++ weather.js
@@ -0,0 +1,41 @@
+"use strict";
+const fs = require("fs");
+const argv = require("yargs").argv;
+var weather = require("openweather-apis");
+var dotenv = require("dotenv").config();
+var dir = "./dist";
+
+if (dotenv.error) {
+  throw dotenv.error;
+}
+const conf = dotenv.parsed;
+console.log(conf);
+
+console.log("======> args");
+console.log(argv);
+
+weather.setAPPID(conf.openweather);
+weather.setLang("en");
+weather.setCity(argv.city);
+
+fs.readFile(dir + "/weather.json", function (err, data) {
+  if (err) {
+    // get all the JSON file returned from server (rich of info)
+    weather.getAllWeather(function (err, JSONObj) {
+      fs.writeFileSync(
+        dir + "/weather.json",
+        JSON.stringify([{ [argv.city]: JSONObj }])
+      );
+    });
+  } else {
+    const _data = JSON.parse(data);
+    var filtered = _data.filter(function (el) {
+      return !el[argv.name];
+    });
+
+    weather.getAllWeather(function (err, JSONObj) {
+      filtered.push({ [argv.name]: JSONObj });
+      fs.writeFileSync(dir + "/weather.json", JSON.stringify(filtered));
+    });
+  }
+});
