71e503c40c721de4ad78a2db6480dd59303e6574
MoatazAbdAlmageed <moataz@wuilt.com>
2020-07-01 19:42:30 +0200

    add modules

---
+++ developers.js
@@ -0,0 +1,109 @@
+"use strict";
+const argv = require("yargs").argv;
+const fs = require("fs");
+
+const dir = "./dist";
+const ADD = "add";
+const REMOVE = "remove";
+
+/***
+ * Contents
+ * writeFile
+ * readFile
+ * using yargs
+ * -  node  index.js add --name='moataz' --age=30
+ * -  node  index.js remove --name='moataz'
+ */
+
+/**
+ * writeFile
+ */
+// fs.writeFile(dir+"file.txt", "writeFile 2 content here", function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("done");
+//   }
+// });
+
+// const fileData = fs.writeFileSync(dir+"file.txt", "file content here");
+/**
+ * readFile
+ */
+// fs.readFile(dir+"file.txt", function (err, data) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("readFile");
+//     // console.log(data);
+//     console.log(data.toString());
+//   }
+// });
+
+// const reader = fs.readFileSync(dir+"file.txt");
+// console.log("readFileSync");
+// console.log(reader.toString());
+
+// // fs.writeFile(dir+"file.txt", "this is data from writeFile", function(err) {
+// //   if (err) {
+// //     console.log(err);
+// //   } else {
+// //     // console.log("done from writeFile");
+// //   }
+// // });
+
+// // fs.readFile(dir+"file.txt", function(err , data) {
+// //   console.log(data.toString());
+// // });
+
+// // fs.writeFileSync(dir+"file.txt", "this is data from writeFileSync");
+// // var data = fs.readFileSync (dir+"file.txt");
+// // console.log(data.toString());
+
+// // console.log(pro[2]);
+
+/**
+ * Read arguments from terminal
+ * we will not use `process.argv` but we will use `yargs`
+ * https://www.npmjs.com/package/yargs
+ * Usage
+ * -    nodemon index.js --ships=4 --distance=22
+ */
+exports.init = function () {
+  console.log("init...");
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+  if (argv._[0] === ADD) {
+    fs.readFile(dir + "/developers.json", function (err, data) {
+      if (err) {
+        fs.writeFile(
+          dir + "/developers.json",
+          JSON.stringify([{ [argv.name]: argv.age }]),
+          function (err) {
+            console.log(err);
+          }
+        );
+      } else {
+        const _data = JSON.parse(data);
+        var filtered = _data.filter(function (el) {
+          return !el[argv.name];
+        });
+        filtered.push({ [argv.name]: argv.age });
+        fs.writeFileSync(dir + "/developers.json", JSON.stringify(filtered));
+      }
+    });
+  } else if (argv._[0] === REMOVE) {
+    fs.readFile(dir + "/developers.json", function (err, data) {
+      if (err) {
+        throw err;
+      }
+      const _data = JSON.parse(data);
+      var filtered = _data.filter(function (el) {
+        return !el[argv.name];
+      });
+
+      fs.writeFileSync(dir + "/developers.json", JSON.stringify(filtered));
+    });
+  }
+};
--- weather.js
+++ weather.js
@@ -1,40 +1,36 @@
 "use strict";
-const fs = require("fs");
 const argv = require("yargs").argv;
+const fs = require("fs");
 var weather = require("openweather-apis");
 var dotenv = require("dotenv").config();
-var dir = "./dist";
-
-if (dotenv.error) {
-  throw dotenv.error;
-}
+const dir = "./dist";
 const conf = dotenv.parsed;
-console.log(conf);
 
-console.log("======> args");
-console.log(argv);
+exports.init = function () {
+  console.log("init...");
 
-weather.setAPPID(conf.openweather);
-weather.setLang("en");
-weather.setCity(argv.city);
+  weather.setAPPID(conf.openweather);
+  weather.setLang("en");
+  weather.setCity(argv.city);
 
-fs.readFile(dir + "/weather.json", function (err, data) {
-  if (err) {
-    // get all the JSON file returned from server (rich of info)
-    weather.getAllWeather(function (err, JSONObj) {
-      fs.writeFileSync(
-        dir + "/weather.json",
-        JSON.stringify([{ [argv.city]: JSONObj }])
-      );
-    });
-  } else {
-    const _data = JSON.parse(data);
-    var filtered = _data.filter(function (el) {
-      return !el[argv.city];
-    });
-    weather.getAllWeather(function (err, JSONObj) {
-      filtered.push({ [argv.city]: JSONObj });
-      fs.writeFileSync(dir + "/weather.json", JSON.stringify(filtered));
-    });
-  }
-});
+  fs.readFile(dir + "/weather.json", function (err, data) {
+    if (err) {
+      // get all the JSON file returned from server (rich of info)
+      weather.getAllWeather(function (err, JSONObj) {
+        fs.writeFileSync(
+          dir + "/weather.json",
+          JSON.stringify([{ [argv.city]: JSONObj }])
+        );
+      });
+    } else {
+      const _data = JSON.parse(data);
+      var filtered = _data.filter(function (el) {
+        return !el[argv.city];
+      });
+      weather.getAllWeather(function (err, JSONObj) {
+        filtered.push({ [argv.city]: JSONObj });
+        fs.writeFileSync(dir + "/weather.json", JSON.stringify(filtered));
+      });
+    }
+  });
+};
